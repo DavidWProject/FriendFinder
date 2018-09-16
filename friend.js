fef0a41e61cb978349279012b176ce1d1cccebb7
David Wong <davidwong@Davids-MBP-2.home>
2018-09-15 21:13:32 -0400

    added more people

--- friend.js
+++ friend.js
@@ -1,20 +1,163 @@
-var friendList = [
+var friendList = [{
+        name: 'David Wong',
+        profilePic: 'https://davidwproject.github.io/images/profile-img.png',
+        scores: [
+            "3",
+            "4",
+            "4",
+            "1",
+            "3",
+            "4",
+            "5",
+            "2",
+            "3",
+            "1"
+        ]
+    },
     {
-      name: 'David Wong',
-      profilePic: 'https://davidwproject.github.io/images/profile-img.png',
-      scores: [
-        "3",
-        "4",
-        "4",
-        "1",
-        "3",
-        "4",
-        "5",
-        "2",
-        "3",
-        "1"
-      ]
+        name: 'Homer Simpson',
+        profilePic: 'https://pbs.twimg.com/profile_images/725013638411489280/4wx8EcIA.jpg',
+        scores: [
+            "3",
+            "3",
+            "3",
+            "3",
+            "3",
+            "3",
+            "3",
+            "2",
+            "3",
+            "1"
+        ]
+    },
+    {
+        name: 'Marge Simpson',
+        profilePic: 'https://images.immediate.co.uk/volatile/sites/3/2016/03/Simpsons_g2013_R1_marge-04bf0d1.jpg?quality=90&resize=768,574',
+        scores: [
+            "4",
+            "5",
+            "2",
+            "1",
+            "3",
+            "5",
+            "2",
+            "3",
+            "4",
+            "2"
+        ]
+    },
+    {
+        name: 'Bart Simpson',
+        profilePic: 'https://i.pinimg.com/originals/d3/4e/ca/d34eca644b13450535332d70595f77ee.png',
+        scores: [
+            "5",
+            "5",
+            "1",
+            "2",
+            "3",
+            "4",
+            "5",
+            "4",
+            "3",
+            "2"
+        ]
+    },
+    {
+        name: 'Lisa Simpson',
+        profilePic: 'https://vignette.wikia.nocookie.net/simpsons/images/2/21/IMG_6089.gif/revision/latest?cb=20161017184724',
+        scores: [
+            "5",
+            "4",
+            "3",
+            "2",
+            "1",
+            "2",
+            "3",
+            "4",
+            "5",
+            "1"
+        ]
+    },
+    {
+        name: 'Moe Tavern',
+        profilePic: 'https://pbs.twimg.com/profile_images/94930361/moe_400x400.png',
+        scores: [
+            "3",
+            "4",
+            "4",
+            "1",
+            "3",
+            "4",
+            "5",
+            "2",
+            "3",
+            "5"
+        ]
+    },
+    {
+        name: 'Barney',
+        profilePic: 'https://upload.wikimedia.org/wikipedia/en/thumb/d/de/Barney_Gumble.png/220px-Barney_Gumble.png',
+        scores: [
+            "1",
+            "2",
+            "3",
+            "4",
+            "5",
+            "4",
+            "3",
+            "2",
+            "1",
+            "2"
+        ]
+    },
+    {
+        name: 'David Wong',
+        profilePic: 'https://davidwproject.github.io/images/profile-img.png',
+        scores: [
+            "3",
+            "4",
+            "4",
+            "1",
+            "3",
+            "4",
+            "5",
+            "2",
+            "3",
+            "1"
+        ]
+    },
+    {
+        name: 'Ned Flanders',
+        profilePic: 'https://vignette.wikia.nocookie.net/simpsons/images/d/d6/Ned_Flanders_2.png/revision/latest?cb=20141024230457',
+        scores: [
+            "1",
+            "1",
+            "1",
+            "1",
+            "1",
+            "1",
+            "1",
+            "1",
+            "1",
+            "1"
+        ]
+    },
+    {
+        name: 'Sideshow Bob',
+        profilePic: 'http://www.theandrewblog.net/wp-content/uploads/2016/03/sbr21.jpg',
+        scores: [
+            "5",
+            "5",
+            "5",
+            "5",
+            "5",
+            "5",
+            "5",
+            "5",
+            "5",
+            "5"
+        ]
     }
-  ]
-  
-  module.exports = friendList;
+]
+
+module.exports = friendList;
